b299f067fb4bb1717bdbde618219f4764ac20c9c
gaodonghao <844810161@qq.com>
2018-11-12 23:31:18 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import fastclick from "fastclick";
+import VueAwesomeSwiper from 'vue-awesome-swiper' //轮播图
 
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css' //轮播图css
 
 Vue.config.productionTip = false;
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 new Vue({
 	router,
