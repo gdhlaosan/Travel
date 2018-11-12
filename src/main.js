b70314295afe35f4f4a30a5449819cc559ee82b8
gaodonghao <844810161@qq.com>
2018-11-12 17:30:07 +0800

    '项目初始化'

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import fastclick from "fastclick";
+
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false;
+fastclick.attach(document.body)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount("#app");
---
+++ src/pages/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="home">
+		高东浩
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'home',
+}
+</script>
+
+<style lang="stylus">
+#home
+	font-size 50px
+</style>
